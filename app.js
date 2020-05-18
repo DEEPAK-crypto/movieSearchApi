6eb83f6b982930d25f05aa6bbc1fceb616f07d02
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-18 19:45:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var request = require("request");
 app.set("view engine", 'ejs');
-
+app.use(express.static("public"));
 app.get('/results', function(req, res) {
     request('http://www.omdbapi.com/?s=harry+potter&apikey=thewdb', function(error, request, body) {
         if (!error && request.statusCode == 200) {
